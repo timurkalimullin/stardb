61c44bc8a5730188a51e304953e102c343d02de1
Timur Kalimullin <timur_kalimullin@list.ru>
2020-08-11 01:08:00 +0500

    feat: add swapi service, random planet component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-eclipse-j4f16ga2l48"><div className="ldio-dzueg9teuww">
+      <div></div>
+    </div></div>
+  );
+};
+
+export default Spinner;
